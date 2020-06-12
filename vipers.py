40b2e4d413f96a1b8743bdaec40f009fb0feedcf
Xvezda <xvezda@naver.com>
2020-06-12 22:15:40 +0900

    Add vipers.py

---
+++ vipers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Xvezda <xvezda@naver.com>
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
